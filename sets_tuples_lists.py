f38ba23b2a5b45f2070e0ff9782dca11931e5684
Lara <alara50@cps.k12.il.us>
2024-09-26 08:32:48 -0500

    first commit

---
+++ sets_tuples_lists.py
@@ -0,0 +1,25 @@
+#collestion = single "variable" used to store multiple values
+# List =[] ordered and changeable. Duplicates OK
+# Set = {} unordered and immutable, but Add/Remove OK. No duplicates
+# Tuple= () ordered abd unchageable. Duplicates OK. FASTER
+
+fruits= ["apple", "orange", "banana", "coconut", "Pear", "blueberries"]
+#print(dir(fruits)) #prints out the documentaion 
+#print(help(fruits))
+#print(len(fruits))
+#print("apple" in fruits) #says if its true or not 
+
+
+#fruits[1]="pineapple"# i can resign values using this 
+#fruits.append("pineapple") #move variables
+#fruits.remove("apple") #
+#fruits.insert(0, "pineapple")
+#fruits.sort()
+#fruits.reverse()
+#fruits.clear()
+
+
+#print(fruits[::-1])
+
+for fruit in fruits:
+    print(fruit)
