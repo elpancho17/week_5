3f3b162be3eaa824b874fdc2b9a367d98bedfdaa
Lara <alara50@cps.k12.il.us>
2024-10-01 09:04:15 -0500

    Thanks Max

--- sets_tuples_lists.py
+++ sets_tuples_lists.py
@@ -3,7 +3,7 @@
 # Set = {} unordered and immutable, but Add/Remove OK. No duplicates
 # Tuple= () ordered abd unchageable. Duplicates OK. FASTER
 
-#fruits= ["apple", "orange", "banana", "coconut", "Pear", "blueberries"]
+fruits= ["apple", "orange", "banana", "coconut", "Pear", "blueberries"]
 #print(dir(fruits)) #prints out the documentaion 
 #print(help(fruits))
 #print(len(fruits))
@@ -17,6 +17,33 @@
 #fruits.sort() #sorts 
 #fruits.reverse()
 #fruits.clear()
+
+fruits= {"apple", "orange", "banana", "coconut", "Pear", "blueberries"}
+#print(dir(fruits)) #prints out the documentaion 
+#print(help(fruits))
+#print(len(fruits))
+#print("apple" in fruits) #says if its true or not 
+
+# fruits.remove("apple") #removes something
+# fruits.add("pickles")
+# fruits.add("tomatoes") #add something
+# fruits.pop() #pops of the first one but it random
+# print(fruits)
+
+
+fruits= ("apple", "orange", "banana", "coconut", "Pear", "blueberries")
+#print(dir(fruits)) #prints out the documentaion 
+#print(help(fruits))
+#print(len(fruits))
+#print("apple" in fruits) #says if its true or not 
+
+# print(fruits.index("apple"))
+# print(fruits.count("coconut"))
+
+# print(fruits)
+# for fruit in fruits:
+#     print(fruit)
+
 
 
 #print(fruits[::-1])
@@ -62,29 +89,66 @@
 #challenge
 # create a list of friends
 # make sure the intital list is none
-friends=[]
-#add a new friend to the list, add at least 5 friends.
-friendAdd= input("add a new friend please: ")
-friends.append(friendAdd)
-print(len(friends))
-print(friends)
-friendAdd= input("add a new friend please: ")
-friends.append(friendAdd)
-print(len(friends))
-print(friends)
-friendAdd= input("add a new friend please: ")
-friends.append(friendAdd)
-print(len(friends))
-print(friends)
-friendAdd= input("add a new friend please: ")
-friends.append(friendAdd)
-print(len(friends))
-print(friends)
-friendAdd= input("add a new friend please: ")
-friends.append(friendAdd)
-print(len(friends))
-print(friends)
-friends.remove("cory")
-friends.insert(2,"maxwell")
-print(friends)
-print("Cloud" in friends)
+# friends=[]
+# #add a new friend to the list, add at least 5 friends.
+# friendAdd= input("add a new friend please: ")
+# friends.append(friendAdd)
+# print(len(friends))
+# print(friends)
+# friendAdd= input("add a new friend please: ")
+# friends.append(friendAdd)
+# print(len(friends))
+# print(friends)
+# friendAdd= input("add a new friend please: ")
+# friends.append(friendAdd)
+# print(len(friends))
+# print(friends)
+# friendAdd= input("add a new friend please: ")
+# friends.append(friendAdd)
+# print(len(friends))
+# print(friends)
+# friendAdd= input("add a new friend please: ")
+# friends.append(friendAdd)
+# print(len(friends))
+# print(friends)
+# friends.remove("cory")
+# friends.insert(2,"maxwell")
+# print(friends)
+# print("Cloud" in friends)
+
+
+#dictionary= a collection of {key:value} pairs ordered and changeable. No duplicates
+capitals= {"USA": "Washington D.C",
+           "Inda": "New Delhi",
+           "China": "Beijing",
+           "Russia": "Moscow"}
+
+# print(dir(capitals))
+# print(help(capitals))
+# print(capitals.get("USA"))
+#print(capitals.get("Japan"))
+
+# if capitals.get("Japan"):
+#     print("that capital exist")
+# else:
+#     print("that capital doesn't exist")
+
+# capitals.update({"Germany": "Berlin"})
+# capitals.pop("China")
+# capitals.clear()
+
+# keys = capitals.keys()
+# for key in capitals.keys():
+#     print(key)
+
+# values = capitals.values()
+# for value in capitals.values():
+#     print(value)
+
+# items = capitals.items()
+# for key, value in capitals.items():
+#     print(f"{key}: {value}")
+
+# print(capitals)
+
+#Courtasy of Maxwell
